1690aa43ad0036fa58648b2cfb6e137421ff5006
Yi-Nung Yeh <yi-nungyeh@Yi-Nungs-MacBook-Air.local>
2025-03-18 15:31:54 -0700

    Initial commit - Neural Network Visualizer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NeuralNetworkFunctionApproximator from './components/NeuralNetworkFunctionApproximator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Neural Network Function Approximation</h1>
       </header>
+      <main className="App-main">
+        <NeuralNetworkFunctionApproximator />
+      </main>
+      <footer className="App-footer">
+        <p>Created with React</p>
+      </footer>
     </div>
   );
 }
